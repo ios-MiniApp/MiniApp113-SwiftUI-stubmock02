c67e3b4a07f6de9f72bbcd67444f453151f1c2c5
Kota <maedakoutaa@gmail.com>
2022-11-28 07:23:42 +0900

    Initial Commit

---
+++ MiniApp113-SwiftUI-stubmock02/MiniApp113_SwiftUI_stubmock02App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp113_SwiftUI_stubmock02App.swift
+//  MiniApp113-SwiftUI-stubmock02
+//
+//  Created by 前田航汰 on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp113_SwiftUI_stubmock02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
